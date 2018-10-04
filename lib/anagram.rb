d2c14e827d5c28815c41c7782108201040053be1
Stef Claus <stef.claus@gmail.com>
2018-10-04 23:55:00 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -15,6 +15,9 @@ class Anagram
       
       if self.sort == word_list.sort 
       
+      find_anagram_in_this_string.find_all do |word| 
+      if word.split("").sort == self.word_to_analyze.split("").sort 
+        word 
     else 
       nil 
     end 

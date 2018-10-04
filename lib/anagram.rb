3c30bb6b968e9bc577d580451a9c752beff5a3ff
Stef Claus <stef.claus@gmail.com>
2018-10-04 23:32:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
     @word =word 
   end 
   
-  def match(@word, word_list)
+  def match(word, word_list)
     word_list.each do |list_word|
       word.split("")
       list_word.split("")

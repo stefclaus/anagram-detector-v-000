0b260de47a41b3e7e6eb346a388212bcee55fdfa
Stef Claus <stef.claus@gmail.com>
2018-10-04 23:34:40 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
       array = []
       array << list_word
     end 
-       list_word.split("")
+       array.split("")
       word.split("")
       if word.sort == list_word.sort 
       list_word 

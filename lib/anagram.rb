f3fb894ca20045bc160f3f4ca1fa4e3d02bd0b81
Stef Claus <stef.claus@gmail.com>
2018-10-04 23:56:02 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,16 +8,9 @@ class Anagram
   end 
   
   def match(word_list)
-      word_list.each do |word|
-    end 
-       word.split("")
-       self.split("")
-      
-      if self.sort == word_list.sort 
-      
-      find_anagram_in_this_string.find_all do |word| 
-      if word.split("").sort == self.word_to_analyze.split("").sort 
-        word 
+      word_list.find_all do |word|
+      if word.split("").sort == self.split("").sort
+      word
     else 
       nil 
     end 
